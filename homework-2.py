d499200e856877dba6846e3a60fb5767a1151365
dedasheniirl <dachiagladze20@gmail.com>
2025-10-31 08:09:36 +0400

    Reversed String

--- homework-2.py
+++ homework-2.py
@@ -1,13 +1,2 @@
-def DictonaryFunction(x):
-    result = {}
-
-    for char in x:
-        if char in result:
-            result[char] += 1
-        else:
-            result[char] = 1
-
-    return result
-
-
-print(DictonaryFunction("hello"))
+x = input()
+print(x[::-1])
